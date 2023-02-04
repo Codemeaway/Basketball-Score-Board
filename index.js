aa90169a35f986bece515c112af2578a0e1b39c6
Aysenur Fidan <120117444+Codemeaway@users.noreply.github.com>
2023-02-05 02:31:40 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+let scoreHomeEl = document.getElementById("score-home");
+//let homeButton1 = document.getElementById("home-button-1");
+let scoreHome = 0;
+
+function add1() {
+  scoreHome += 1;
+  scoreHomeEl.textContent = scoreHome;
+}
+function add2() {
+  scoreHome += 2;
+  scoreHomeEl.textContent = scoreHome;
+}
+function add3() {
+  scoreHome += 3;
+  scoreHomeEl.textContent = scoreHome;
+}
+
+let scoreGuestEl = document.getElementById("score-guest");
+let scoreGuest = 0;
+
+function add1Guest() {
+  scoreGuest += 1;
+  scoreGuestEl.textContent = scoreGuest;
+}
+function add2Guest() {
+  scoreGuest += 2;
+  scoreGuestEl.textContent = scoreGuest;
+}
+function add3Guest() {
+  scoreGuest += 3;
+  scoreGuestEl.textContent = scoreGuest;
+}
